6ce08af80f803aa22fd66c25b37c4f1322348f9b
Nosaj <urghlol9@gmail.com>
2026-09-10 14:46:10 +0700

    update kuis 1

--- ppbo_kuis1_1124014_jasongabriel.java
+++ ppbo_kuis1_1124014_jasongabriel.java
@@ -1,24 +1,38 @@
-enum connectable{
-    WIFI, BLUETOOTH
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// enum connectable{
+//     WIFI, BLUETOOTH
+// }
+
+// enum lockable{
+//     Terbuka, Terkunci
+// }
+
+// enum switchable{
+//     Menyala, Mati
+// }
+
+
+interface connectable{
+    String konek();
+}
+interface lockable{
+    String kunci();
+}
+interface switchable{
+    String turning();
 }
 
-enum lockable{
-    Terbuka, Terkunci
-}
-
-enum switchable{
-    Menyala, Mati
-}
-
-
-abstract class elektronik {
+abstract class SmartDevice {
     private String id;
     private String nama;
     private int daya;
     private String channel;
     private int volume;
 
-    public elektronik (String id, String nama, int daya, String channel, int volume){
+    public SmartDevice (String id, String nama, int daya, String channel, int volume){
         this.id = nama;
         this.nama = nama;
         this.daya = daya;
@@ -41,18 +55,76 @@ abstract class elektronik {
     }
 }
 
-abstract class SmartTV extends elektronik {
+abstract class SmartTV extends SmartDevice {
+
+    public SmartTV (String id, String nama, int daya, String channel, int volume){
+        super(id, nama, daya, channel, volume);
+    }
+
+    @Override 
+    public String konek(){
+        return "Koneksi: WIFI"
+    }
+    @Override 
+    public String switch(){
+        return "Status: Menyala"
+    }
+    @Override 
+    public String getPrintDetail(){
+        return "Smart TV" + nama + "";
+    }
+}
+
+abstract class SmartSpeaker extends SmartDevice {
     private final connectable konektifitas;
     private final switchable turning;
 
-    public SmartTV (String id, String nama, int daya, String channel, int volume, connectable, switchable){
-        super(id, nama, daya, channel, volume,);
-        this.konektifitas = konektifitas;
-        this.turning = turning;
-
+    public SmartSpeaker (String id, String nama, int daya, String channel, int volume){
+        super(id, nama, daya, channel, volume);
 
     }
+
+
+    @Override 
+    public String konek(){
+        return "Koneksi: BLUETOOTH"
+    }
+    @Override 
+    public String switch(){
+        return "Status: Menyala"
+    }
+    @Override 
+    public String getPrintDetail(){
+        return "Smart Speaker" + nama + "";
+    }
 }
+
+
+abstract class SmartDoor extends SmartDevice {
+    private final connectable konektifitas;
+    private final switchable turning;
+
+    public SmartDoor (String id, String nama, int daya, String channel, int volume){
+        super(id, nama, daya, channel, volume);
+
+    }
+
+
+    @Override 
+    public String konek(){
+        return "Koneksi: WIFI"
+    }
+    @Override 
+    public String switch(){
+        return "Status: Menyala"
+    }
+    @Override 
+    public String getPrintDetail(){
+
+        return "Smart Door" + nama + "";
+    }
+}
+
 
 public class ppbo_kuis1_1124014_jasongabriel {
     public static void main(String[] args){
